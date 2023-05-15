cdb329636c4a60381bd79d579679843a1c027ae4
nathanlu <nathanlumax@gmail.com>
2023-05-15 16:52:23 -0500

    add footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,4 @@
+<div class="w-full flex items-center justify-center text-sm bg-white py-5 text-dark font-semibold border-t border-gray-200">
+    <h4> Created by Nathan Lu - © 2023
+    </h4>
+</div>
