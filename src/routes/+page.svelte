6c820c94f9fe9af239dadc5d035ef3ccafcc2fbb
nathanlu <nathanlumax@gmail.com>
2023-05-14 23:04:40 -0500

    add custom colors, fonts, and favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="mt-3">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<h1 class="text-3xl font-bold underline">
+<h1 class="text-primary text-3xl font-bold underline">
     Hello world!
 </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#0F9CDA',
+                'secondary': '#E5E5E5',
+                'dark': '#09293A',
+                'white': '#F5F5F5',
+                'orange': '#E9AD03',
+                'green': '#9FBB46',
+                'dark-blue': '#063E57',
+                'modal-bg': '#DFF0F8',
+                'card-bg': '#ECF4F8'
+            }
+        },
+    },
+    plugins: [],
 }
 
