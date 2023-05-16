7cb4535d581c31a0581844055966a298f97d2e4e
nathanlu <nathanlumax@gmail.com>
2023-05-15 22:24:42 -0500

    remove api endpoints, replace with page server load

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,27 +1,35 @@
-import type {PageServerLoad} from './$types';
 import {kv} from "@vercel/kv";
 import {defaultUserName, defaultUserPoints} from "$lib/constants";
 
-export const load = (async ({cookies}) => {
-    // get the session id from the cookies, and if it doesn't exist, create one using
-    // Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    // then, we need to call the api endpoint to create the user in the KV store
-    const sessionId = cookies.get('session_id');
+import type {PageServerLoad} from './$types';
+
+export const load: PageServerLoad = async ({ cookies }) => {
+    let sessionId = cookies.get('session_id');
 
     if (!sessionId) {
-        const sessionId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-        cookies.set('session_id', sessionId);
-
-        kv.hset(`user:${sessionId}`, {name: defaultUserName, points: defaultUserPoints}).then(() => {
-            console.log("User created");
-        }, (error) => {
-            console.error(error);
+        sessionId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        cookies.set('session_id', sessionId, {
+            expires: new Date('Fri, 31 Dec 9999 23:59:59 GMT'),
         });
+
+        const response = await kv.hexists(`user:${sessionId}`, "points");
+
+        if (response === null) {
+            try {
+                await kv.hmset(`user:${sessionId}`, {
+                    name: defaultUserName,
+                    points: defaultUserPoints
+                });
+            } catch (error) {
+                console.error(error);
+                cookies.delete('session_id');
+            }
+        }
     }
 
     return {
         props: {
             sessionId
         }
-    }
-}) satisfies PageServerLoad;
+    };
+};
