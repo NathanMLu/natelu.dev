91be0d6cc1f7568dbb7949d0ac629a0fe60513c1
nathanlu <nathanlumax@gmail.com>
2023-05-17 22:28:05 -0500

    make mobile friendly, add better spacing

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -3,13 +3,13 @@
     import wavesPlaceholder from "$lib/images/waves-placeholder.png";
 </script>
 
-<div class="flex items-center justify-center mt-20">
-    <div class="flex justify-center w-1/2">
+<div id="hero-container" class="flex flex-col items-center justify-center lg:flex-row">
+    <div class="flex justify-center w-full lg:w-1/2">
         <div class="h-min">
             <h1 class="font-bold text-7xl">
                 Nate Lu
             </h1>
-            <h3 class="font-semibold text-lg mt-2 w-60">
+            <h3 class="font-semibold text-lg mt-2 w-80 lg:w-60">
                 Hello! I'm a Software Engineer, Student, and Entrepreneur.
             </h3>
             <a href="#contact">
@@ -20,11 +20,16 @@
         </div>
     </div>
 
-    <div class="flex justify-center align-baseline w-1/2">
-        <img class="w-3/5" src={wavesPlaceholder} alt="waves placeholder">
+    <div class="flex justify-center align-baseline w-full mt-10 lg:mt-0 lg:w-1/2">
+        <img class="w-2/3 lg:w-3/5" src={wavesPlaceholder} alt="waves placeholder">
     </div>
 </div>
 
+
 <style>
+    #hero-container {
+        margin-top: 15vh;
+        margin-bottom: 10vh;
+    }
 
 </style>
