2d478e09911293d8994c39e401696a21317bf33e
nathanlu <nathanlumax@gmail.com>
2023-06-05 18:56:38 -0500

    add modal

--- src/lib/components/River.svelte
+++ src/lib/components/River.svelte
@@ -4,10 +4,11 @@
     import Button from "$lib/components/Button.svelte";
     import {SCALE_RIVER_HEIGHT} from "$lib/models/constants";
 
-    import {onMount} from 'svelte';
+    import {afterUpdate, onMount} from 'svelte';
 
     let canvas: HTMLCanvasElement;
     let ctx: CanvasRenderingContext2D;
+    let showModal = false;
 
     onMount(() => {
         handleResize();
@@ -16,8 +17,22 @@
         drawScreen();
     });
 
+    afterUpdate(() => {
+        if (showModal) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+
+        handleResize();
+    });
+
     const openShop = () => {
-        console.log('open shop');
+        showModal = true;
+    }
+
+    const closeShop = () => {
+        showModal = false;
     }
 
     const drawScreen = () => {
@@ -36,12 +51,28 @@
     }
 
     const handleResize = () => {
-        canvas.width = window.innerWidth;
+        canvas.width = document.body.clientWidth;
         canvas.height = window.innerHeight * SCALE_RIVER_HEIGHT;
     }
 </script>
 
 <svelte:window on:resize={handleResize}/>
+{#if showModal}
+    <div class="z-50 fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+        <div class="bg-light-blue rounded-3xl w-1/2">
+            <div class="flex flex-col items-center justify-center p-8">
+                <h1 class="text-3xl font-bold text-dark mb-6">
+                    Nate's Shop
+                </h1>
+                <p class="text-dark mb-6">
+                    Nate's shop is currently under construction. Please check back later!
+                </p>
+                <Button color="primary" on:click={closeShop}>Close</Button>
+            </div>
+        </div>
+    </div>
+{/if}
+
 <div id="river">
     <div class="bg-gradient-to-b from-secondary to-green">
         <div class="flex flex-col items-start justify-center lg:px-24 px-12 pt-32 pb-28">
@@ -53,6 +84,6 @@
             <Button color="primary" on:click={openShop}>Nate's Shop</Button>
         </div>
     </div>
-        <canvas id="riverbed" class="bg-green" bind:this={canvas}>
-        </canvas>
+    <canvas id="riverbed" class="bg-green" bind:this={canvas}>
+    </canvas>
 </div>
