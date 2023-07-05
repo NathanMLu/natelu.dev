4004463609db871c523f9f23b3b5ee133023d1e6
nathanlu <nathanlumax@gmail.com>
2023-07-05 02:35:14 -0500

    update go back button

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+    import Button from "$lib/components/Button.svelte";
 </script>
 
-<div class="flex items-center justify-center w-full h-screen">
+<div class="flex flex-col items-center justify-center w-full h-screen">
     <h2 class="text-2xl font-bold text-dark">To be implemented...</h2>
+    <Button color="primary" customClass="mt-4">
+        <a href="/">Go back</a>
+    </Button>
 </div>
