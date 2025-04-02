b3450a554513f2cdb3554e6fe0611f36fdb3b42d
nathanlu <nathanlumax@gmail.com>
2025-04-02 01:33:10 -0500

    add underline and add updated pfp

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,7 +5,7 @@
 
 <div class="w-full flex items-center justify-center text-sm bg-white py-5 text-dark font-semibold border-t border-gray-200">
     <h4>
-        <a href="{links.figma}" target="_blank" rel="noopener noreferrer">Designed</a>
+        <a href="{links.figma}" target="_blank" rel="noopener noreferrer" class="underline underline-offset-2">Designed</a>
 <!--        and <a href="{links.github}" target="_blank" rel="noopener noreferrer">Created</a>-->
         by Nathan Lu - © 2023
     </h4>
