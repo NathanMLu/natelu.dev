57837ab6857266e950d31a3f7344caacfc694371
nathanlu <nathanlumax@gmail.com>
2025-05-11 21:08:31 -0500

    update resume link

--- src/routes/resume/+page.server.ts
+++ src/routes/resume/+page.server.ts
@@ -1,5 +1,5 @@
 import {redirect} from "@sveltejs/kit";
 
 export function load() {
-    throw redirect(302, 'https://drive.google.com/file/d/1gzWW0nMFLvLOpp9h6z1a7_URUvA_FgIl/view');
+    throw redirect(302, 'https://drive.google.com/file/d/1rZa4k4A6UeTQFPIWnjIWdojiW3Q7Zu6j/view');
 }
